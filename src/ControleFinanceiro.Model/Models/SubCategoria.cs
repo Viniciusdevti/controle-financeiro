4f6ed1cc1d413a8d19cd86a5d8bb037955ebaf5d
viniciusdevti <viniciusdevti@gmail.com>
2022-02-06 21:50:27 -0300

    Adicionando modelo

---
+++ src/ControleFinanceiro.Model/Models/SubCategoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ControleFinanceiro.Model.Models
+
+{
+    [Table("SubCategoria")]
+    public class SubCategoria
+    {
+        [Key]
+        public long IdSubCategoria { get; set; }
+        [Required]
+        public int Nome { get; set; }
+        [Required]
+        public long IdCategoria { get; set; }
+    }
+}
