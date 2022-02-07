764cfb51b024901b2575a469d20c5b8d38a86d80
viniciusdevti <viniciusdevti@gmail.com>
2022-02-06 21:34:24 -0300

    Configurando estrutura inicial do projeto.

---
+++ src/ControleFinanceiro/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iCommercial.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+      
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("homeController");
+        }
+    }
+}
